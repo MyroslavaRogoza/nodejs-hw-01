b48b21af25516d06520e618dcdcdcf601dd211e7
MyroslavaRogoza <miroslavarogozayyy@gmail.com>
2024-05-21 12:21:36 +0300

    add one update

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,5 +1,26 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-export const addOneContact = async () => {};
+const addOneContact = async () => {
+  let createdContactArray = [];
+  const data = await fs.readFile(PATH_DB, 'utf-8');
+  const parsedData = JSON.parse(data);
+    createdContactArray.push(createFakeContact());
+    if (parsedData.length > 0) {
+      const addedContacts = [...parsedData, ...createdContactArray];
+      await fs.writeFile(
+        PATH_DB,
+        JSON.stringify(addedContacts, null, 2),
+        'utf-8',
+      );
+    } else {
+      await fs.writeFile(
+        PATH_DB,
+        JSON.stringify(createdContactArray, null, 2),
+        'utf-8',
+      );
+    }
+};
 
 await addOneContact();
