1cd1af700932f696bc382acef0f59714bcf3e931
MyroslavaRogoza <miroslavarogozayyy@gmail.com>
2024-05-21 12:48:25 +0300

    count contacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,14 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+    let contactsSum = 0;
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const parsedData = JSON.parse(data);
+    for (const contact of parsedData) {
+        contactsSum += 1;
+    }
+    return contactsSum;
+};
 
 console.log(await countContacts());
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,5 +1,10 @@
+import fs from 'node:fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const getAllContacts = async () => {};
+export const getAllContacts = async () => {
+  const data = await fs.readFile(PATH_DB, 'utf-8');
+  const parsedData = JSON.parse(data);
+  return parsedData;
+};
 
 console.log(await getAllContacts());
